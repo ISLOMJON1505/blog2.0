b21674c6647c122af88f7fa568a2c6054a58b8c7
ISLOMJON SIDDIQILLAYEV <60554852+ISLOMJON1505@users.noreply.github.com>
2023-01-25 01:42:13 +0500

    news1

--- blo2/settings.py
+++ blo2/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str("SECRET_KEY")
+SECRET_KEY = "django-insecure-e$db!80_hh(j^-*)32ki8+z=88(j^z#l_m(6#q@n_q0ibz1zbh"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
